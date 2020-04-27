a8f1a041f1420700ff1081870c6ffebf565a3fa1
Ahmad Danial <49461314+ahmaddanialmohd@users.noreply.github.com>
2020-04-28 05:49:22 +0800

    Included updated version of the SQL query.
    This is based on the discussion in https://community.mattermost.com/core/pl/tk3wo17jtirp9fn8i7ab6uei5w. I can confirm that the query is working as expected on versions 5.18 and above.

--- get_last_login_time_all_users.sql
+++ get_last_login_time_all_users.sql
@@ -1,5 +1,5 @@
 # The following MySQL query retrieves the last login time
-# for all users in Mattermost
+# for all users in Mattermost 5.17 and below
 SELECT 
    u.UserName, u.Email, FROM_UNIXTIME((lastlogin.LastLogin/1000)) as last_login_date
 FROM
@@ -14,9 +14,27 @@ GROUP BY
    UserId) lastlogin
 ON 
    u.Id = lastlogin.UserId;
-   
+
+# The following MySQL query retrieves the last login time
+# for all users in Mattermost 5.18 and above
+
+SELECT
+   u.UserName, u.Email, FROM_UNIXTIME((lastlogin.LastLogin/1000)) as last_login_date 
+FROM 
+   Users u
+INNER JOIN 
+   (SELECT UserId, MAX(CreateAt) as LastLogin 
+FROM 
+    Audits
+WHERE 
+    Audits.action = "/api/v4/users/login" AND Audits.extrainfo LIKE "success%" 
+GROUP BY 
+    UserId) lastlogin 
+ON
+   u.Id = lastlogin.UserId ORDER BY last_login_date DESC;
+
 # The following PostgreSQL query retrieves the last login time
-# for all users in Mattermost
+# for all users in Mattermost Mattermost 5.17 and below
 
 SELECT 
    u.UserName, u.Email, to_timestamp((lastlogin.LastLogin/1000)) as last_login_date
@@ -32,3 +50,21 @@ GROUP BY
    UserId) lastlogin
 ON 
    u.Id = lastlogin.UserId;
+
+# The following PostgreSQL query retrieves the last login time
+# for all users in Mattermost Mattermost 5.18 and above
+
+SELECT 
+   u.UserName, u.Email, to_timestamp((lastlogin.LastLogin/1000)) as last_login_date
+FROM 
+   Users u
+INNER JOIN
+   (SELECT UserId, MAX(CreateAt) as LastLogin 
+FROM 
+   Audits 
+WHERE 
+   Audits.action = '/api/v4/users/login' AND Audits.extrainfo LIKE "success%" 
+GROUP BY 
+   UserId) lastlogin
+ON 
+   u.Id = lastlogin.UserId ORDER BY last_login_date DESC;
